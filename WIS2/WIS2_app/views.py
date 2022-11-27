ed9438e084bfbd776f51c96caecba3ef2286bfff
pako <xkaras38@vutbr.cz>
2022-11-27 15:50:29 +0100

    .

--- WIS2/WIS2_app/views.py
+++ WIS2/WIS2_app/views.py
@@ -22,46 +22,7 @@ def user(request: HttpRequest) -> HttpResponse:
   return render(request, "WIS2_app/user/user.html", user_kind)
 
 
-@login_required
-def create_termin(request: HttpRequest, termin_type: str, course_uid: str):
-  user_kind = get_user_kind(request)
-  termin_type2form = {'lecture': CreateLectureForm,
-                      'practice_lecture': CreatePracticeLectureForm,
-                      'exam': CreateExamForm,
-                      'project': CreateProjectForm}
-  termin_type2cz = {'lecture': 'přednášky',
-                    'practice_lecture': 'cvičení',
-                    'exam': 'zkoušky',
-                    'project': 'projektu'}
-  if request.method == 'POST':
-    form = termin_type2form[termin_type](request.POST)
-    if form.is_valid():
-      form.save(course_uid)
-      return redirect('/courses/detail/' + course_uid)
-  else:
-    form = termin_type2form[termin_type]()
-  try:
-    course = Course.objects.get(UID__exact=course_uid)
-  except django.core.exceptions.ObjectDoesNotExist:
-    return redirect('/courses/')
-
-  return render(request, "WIS2_app/user/create_termin.html", {'form': form,
-                                                              'course_name': course.name,
-                                                              'termin_type': termin_type2cz[termin_type],
-                                                              **user_kind})
-
-
-def delete_termin(request: HttpRequest, course_uid: str, termin_uid: str) -> HttpResponse:
-  try:
-    _ = Course.objects.get(UID__exact=course_uid)
-    termin = Termin.objects.get(ID__exact=termin_uid)
-  except django.core.exceptions.ObjectDoesNotExist:
-    return redirect(f'/courses/detail/{course_uid}/')
-
-  termin.delete()
-  return redirect(f'/courses/detail/{course_uid}')
-
-
+# KURZY stuff general
 def courses(request: HttpRequest) -> HttpResponse:
     user_kind = get_user_kind(request)
     registered_courses = []
@@ -86,6 +47,56 @@ def courses(request: HttpRequest) -> HttpResponse:
                                                      'registered_course_list': registered_courses,
                                                      **user_kind})
 
+
+@login_required
+def courses_create(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = CreateCourseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            _garant = Garant()
+            _garant.CourseUID = Course.objects.get(Q(UID=form.cleaned_data['uid']))
+            _garant.UserUID = request.user
+            _garant.save()
+    else:
+        form = CreateCourseForm()
+
+    return render(request, "WIS2_app/user/create_course.html", {'form': form})
+
+
+def courses_detail(request: HttpRequest, course_uid: str) -> HttpResponse:
+    course_query_res = Course.objects.filter(UID__exact=course_uid).all()
+
+    if not len(course_query_res):
+        return redirect("/courses/")
+    if request.POST.get("add"):
+        return redirect("/courses/create_termin/" + course_uid)
+
+    period_terms = (TerminPeriod.objects.
+                    select_related('TerminID').
+                    filter(TerminID__CourseUID__exact=course_uid))
+
+    single_terms = (TerminSingle.objects.
+                    select_related('TerminID').
+                    filter(TerminID__CourseUID__exact=course_uid))
+
+    course = course_query_res[0]
+    print(course)
+
+    exam_list = single_terms.filter(kind__exact="EXM").all()
+    project_list = single_terms.filter(kind__exact="PRJ").all()
+    lecture_list = period_terms.filter(kind__exact="LEC").all()
+    practice_lecture_list = period_terms.filter(kind__exact="PLEC").all()
+    return render(request, "WIS2_app/course_details.html",
+                  {'user': True,
+                   'course': course,
+                   'exam_list': exam_list,
+                   'project_list': project_list,
+                   'lecture_list': lecture_list,
+                   'practice_lecture_list': practice_lecture_list})
+
+
+# Kurzy interakce s uzivatelem
 @login_required
 def my_courses(request: HttpRequest) -> HttpResponse:
     user_kind = get_user_kind(request)
@@ -99,6 +110,9 @@ def my_courses(request: HttpRequest) -> HttpResponse:
 
     # get all existing courses
     return render(request, "WIS2_app/user/my_courses.html", {'registered_course_list': registered_courses, **user_kind})
+
+
+
 
 @login_required
 def courses_join(request: HttpRequest, course_uid) -> HttpResponse:
@@ -144,95 +158,7 @@ def courses_leave(request: HttpRequest, course_uid) -> HttpResponse:
     return redirect('/courses/')
 
 
-@login_required
-def courses_create(request: HttpRequest) -> HttpResponse:
-    if request.method == 'POST':
-        form = CreateCourseForm(request.POST)
-        if form.is_valid():
-            form.save()
-            _garant = Garant()
-            _garant.CourseUID = Course.objects.get(Q(UID=form.cleaned_data['uid']))
-            _garant.UserUID = request.user
-            _garant.save()
-    else:
-        form = CreateCourseForm()
-
-    return render(request, "WIS2_app/user/create_course.html", {'form': form})
-
-
-@login_required
-def course_termin_create(request: HttpRequest, course_uid) -> HttpResponse:
-    if request.method == 'POST':
-        form = CreateTerminForm(request.POST)
-        if form.is_valid():
-            #ci ideme periodicky termin alebo single
-            if request.POST.get("periodic"):
-                form.save("periodic", course_uid)
-                return redirect('/courses/detail/' + course_uid)
-            if request.POST.get("single"):
-                form.save("single", course_uid)
-                return redirect('/courses/detail/' + course_uid)
-    else:
-        form = CreateTerminForm()
-
-    return render(request, "WIS2_app/user/create_termin.html", {'form': form})
-
-
-@login_required
-def new_course(request: HttpRequest) -> HttpResponse:
-    if request.method == 'POST':
-        pass
-    else:
-        pass
-
-
-def courses_detail(request: HttpRequest, course_uid: str) -> HttpResponse:
-    course_query_res = Course.objects.filter(UID__exact=course_uid).all()
-
-    if not len(course_query_res):
-        return redirect("/courses/")
-    if request.POST.get("add"):
-        return redirect("/courses/create_termin/" + course_uid)
-
-    period_terms = (TerminPeriod.objects.
-                    select_related('TerminID').
-                    filter(TerminID__CourseUID__exact=course_uid))
-
-    single_terms = (TerminSingle.objects.
-                    select_related('TerminID').
-                    filter(TerminID__CourseUID__exact=course_uid))
-
-    course = course_query_res[0]
-    print(course)
-
-    exam_list = single_terms.filter(kind__exact="EXM").all()
-    project_list = single_terms.filter(kind__exact="PRJ").all()
-    lecture_list = period_terms.filter(kind__exact="LEC").all()
-    practice_lecture_list = period_terms.filter(kind__exact="PLEC").all()
-    return render(request, "WIS2_app/course_details.html",
-                  {'user': True,
-                   'course': course,
-                   'exam_list': exam_list,
-                   'project_list': project_list,
-                   'lecture_list': lecture_list,
-                   'practice_lecture_list': practice_lecture_list})
-
-
-@login_required
-def termins(request: HttpRequest) -> HttpResponse:
-    # get registered courses
-    user_kind = get_user_kind(request)
-    registered_courses = []
-    if user_kind["user"]:
-        # try to extract user courses
-        registered_courses = (Student.objects.
-                              select_related('CourseUID').
-                              filter(UserUID__exact=request.user.id).
-                              all())
-    return render(request, "WIS2_app/courses/course_termins.html", {'user': True,
-                                                                    'course_list': registered_courses})
-
-
+# TODO: Co toto ma delat?
 @login_required
 def termins_course(request: HttpRequest, course_uid):
     user_kind = get_user_kind(request)
@@ -257,3 +183,60 @@ def termins_course(request: HttpRequest, course_uid):
     practice_lecture_list = period_terms.filter(kind__exact="PLEC").all()
 
     return render(request, "WIS2_app/courses/course_termins_detail.html")
+
+###
+# Termin stuff
+###
+@login_required
+def termins(request: HttpRequest) -> HttpResponse:
+    # get registered courses
+    user_kind = get_user_kind(request)
+    registered_courses = []
+    if user_kind["user"]:
+        # try to extract user courses
+        registered_courses = (Student.objects.
+                              select_related('CourseUID').
+                              filter(UserUID__exact=request.user.id).
+                              all())
+    return render(request, "WIS2_app/courses/course_termins.html", {'user': True,
+                                                                    'course_list': registered_courses})
+
+
+@login_required
+def create_termin(request: HttpRequest, termin_type: str, course_uid: str):
+  user_kind = get_user_kind(request)
+  termin_type2form = {'lecture': CreateLectureForm,
+                      'practice_lecture': CreatePracticeLectureForm,
+                      'exam': CreateExamForm,
+                      'project': CreateProjectForm}
+  termin_type2cz = {'lecture': 'přednášky',
+                    'practice_lecture': 'cvičení',
+                    'exam': 'zkoušky',
+                    'project': 'projektu'}
+  if request.method == 'POST':
+    form = termin_type2form[termin_type](request.POST)
+    if form.is_valid():
+      form.save(course_uid)
+      return redirect('/courses/detail/' + course_uid)
+  else:
+    form = termin_type2form[termin_type]()
+  try:
+    course = Course.objects.get(UID__exact=course_uid)
+  except django.core.exceptions.ObjectDoesNotExist:
+    return redirect('/courses/')
+
+  return render(request, "WIS2_app/user/create_termin.html", {'form': form,
+                                                              'course_name': course.name,
+                                                              'termin_type': termin_type2cz[termin_type],
+                                                              **user_kind})
+
+
+def delete_termin(request: HttpRequest, course_uid: str, termin_uid: str) -> HttpResponse:
+  try:
+    _ = Course.objects.get(UID__exact=course_uid)
+    termin = Termin.objects.get(ID__exact=termin_uid)
+  except django.core.exceptions.ObjectDoesNotExist:
+    return redirect(f'/courses/detail/{course_uid}/')
+
+  termin.delete()
+  return redirect(f'/courses/detail/{course_uid}')
