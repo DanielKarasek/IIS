ed538c9a4df69104985d1c87e628cd94636cc738
ddzibela <d.dzibela0000@gmail.com>
2022-11-28 01:27:47 +0100

    make heroku happy

--- WIS2/WIS2/wsgi.py
+++ WIS2/WIS2/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WIS2.settings')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'WIS2.WIS2.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WIS2.WIS2.settings')
 
 application = get_wsgi_application()
